e6220517b7eee04f8d48a0708bb68519c6def0c6
Layman <egrac@ad.unc.edu>
2024-06-27 10:33:50 -0400

    uploading audiofiles and new script

--- code/measuring_sound_levels.r
+++ code/measuring_sound_levels.r
@@ -4,7 +4,7 @@ library(seewave)
 library(stringr)
 library(dplyr)
 
-eno <- readMP3("audiofiles/EnoRiver_0516_Circle2_Clipped.mp3")
+eno <- readMP3("audiofiles/EnoRiver_0516_Circle3_Clipped.mp3")
 
 # visualizing audiofiles with spectrogram (frequency on y and time on x)
 spectro(eno, flim = c(0, 8))
@@ -24,3 +24,12 @@ peakfreq_eno = eno_amp[eno_amp[,2] == max_cicada_amplitude_eno , 1]
 moving_average <- function(x, n = 5) { stats::filter(x, rep(1 / n, n), sides = 2) }
 
 runavg = moving_average(eno_amp[,2], n = 10)
+
+#kljhkjgkjh
+
+# Eno River Circle 3 0516
+eno2 <- readMP3("audiofiles/EnoRiver_0516_Circle3_Clipped.mp3")
+spectro(eno2, flim = c(0,8))
+par(mfrow = c(1,1))
+eno_amp = spec(eno2, flim = c(0, 2))
+abline(v=c(1.0, 1.2), col = 'cyan')

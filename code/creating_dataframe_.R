75adcd388634abd161982d8896454e0dcbd7b870
Layman <egrac@ad.unc.edu>
2024-07-12 10:25:15 -0400

    creating output table for audiofiles

--- code/creating_dataframe_.R
+++ code/creating_dataframe_.R
@@ -1,5 +1,5 @@
 
-sites = c("EnoRiver", "JMill", "ncbg", "PRidge", "UNC")
+sites = c("eno", "jmill", "ncbg", "pridge", "unc")
 
 
 output = data.frame(site = NULL,
@@ -10,9 +10,9 @@ for (s in sites) {
   
   files = list.files(paste0("audiofiles/", s))  
   
-  for (f in files[1:2]) {
+  for (f in files[1:3]) {
     
-    audiofile <-readMP3(f)
+    audiofile <-readMP3(paste0("audiofiles/", s, "/", f)) 
     audiofile_amp = spec(audiofile, flim = c(.97, 1.23))
     max_cicada_amplitude = max(audiofile_amp[audiofile_amp[,1] > .9 & audiofile_amp[,1] < 1.2, 2])
     peakfreq_audiofile = audiofile_amp[audiofile_amp[,2] == max_cicada_amplitude , 1]

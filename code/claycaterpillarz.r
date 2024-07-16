cc5e4b5e1beaba7aba78d23a1d81144587e49b34
Smith <smith96@ad.unc.edu>
2024-07-16 14:50:09 -0400

    connecting to googlesheet

--- code/claycaterpillarz.r
+++ code/claycaterpillarz.r
@@ -5,3 +5,6 @@ library(gsheet)
 library(lubridate)
 
 # new comment
+url = "https://docs.google.com/spreadsheets/d/1hi7iyi7xunriU2fvFpgNVDjO5ERML4IUgzTkQjLVYCo/edit?gid=0#gid=0"
+
+df = gsheet2tbl(url)

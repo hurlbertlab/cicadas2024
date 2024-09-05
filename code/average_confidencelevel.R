7c1be83b916bade4bd5fa546737ff39439808f67
Layman <egrac@ad.unc.edu>
2024-09-05 12:03:12 -0400

    setting up working directory for call results

--- code/average_confidencelevel.R
+++ code/average_confidencelevel.R
@@ -1,2 +1,8 @@
 #creating data frame for average confidence level of each bird species
+
 #need to join sheet with BirdNET results to sheet with call times on it
+wd <- "Z:/Databases/CaterpillarsCount/AudioMoth/BirdNetResults"
+setwd("Z:/Databases/CaterpillarsCount/AudioMoth/BirdNet Results")
+confirmed_calls = read.csv(paste(wd,"bird_calls_confirmed.csv", sep=""))
+
+#need to create a read.csv line of code that will go through each excel sheet with results and rbinds them.

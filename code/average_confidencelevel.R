5b9eaf087993bd5d53c31c6a5c8dec54ec7cefcb
Layman <egrac@ad.unc.edu>
2024-09-05 14:44:32 -0400

    changes to call table

--- code/average_confidencelevel.R
+++ code/average_confidencelevel.R
@@ -2,7 +2,7 @@
 
 #need to join sheet with BirdNET results to sheet with call times on it
 wd <- "Z:/Databases/CaterpillarsCount/AudioMoth/BirdNetResults"
-setwd("Z:/Databases/CaterpillarsCount/AudioMoth/BirdNet Results")
-confirmed_calls = read.csv(paste(wd,"bird_calls_confirmed.csv", sep=""))
-
+setwd("Z:/Databases/CaterpillarsCount/AudioMoth/BirdNetResults")
+confirmed_calls = read.csv(paste(wd,"/bird_calls_confirmed.csv", sep=""))
+#confirmed_calls = read.csv("bird_calls_confirmed")
 #need to create a read.csv line of code that will go through each excel sheet with results and rbinds them.

d2c302f76aa3186b136beff0e95ae160fef38002
Grace Layman <egrac@ad.unc.edu>
2024-10-03 13:49:02 -0400

    adding to avg confidence level code

--- code/average_confidencelevel.R
+++ code/average_confidencelevel.R
@@ -1,5 +1,24 @@
 #creating data frame for average confidence level of each bird species
+library(dplyr)
 
+#sites = c("eno", "jmill", "ncbg", "pridge", "unc")
+
+#confidence_levels = data.frame(site = "ex",
+                               file = "ex",
+                               start = 1,
+                               end = 1, 
+                               scientific_name = "ex", 
+                               common_name = "ex",
+                               confidence = 1)
+
+#for (s in sites) {
+  
+  files = list.files(paste0("data/birdcallsdata"))
+  
+  for (f in files)
+    file <- read.csv(f)
+  
+}
 #need to join sheet with BirdNET results to sheet with call times on it
 wd <- "Z:/Databases/CaterpillarsCount/AudioMoth/BirdNetResults"
 setwd("Z:/Databases/CaterpillarsCount/AudioMoth/BirdNetResults")

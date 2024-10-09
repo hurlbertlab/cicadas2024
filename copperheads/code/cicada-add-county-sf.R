e919013bd54d0931926f981f98cc7118d2c1bcdf
IJBG <ivaragoulden@gmail.com>
2024-10-09 12:13:13 -0400

    fix .gdb

--- copperheads/code/cicada-add-county-sf.R
+++ copperheads/code/cicada-add-county-sf.R
@@ -36,21 +36,8 @@ cicada <- st_read(dsn = gdb_path) %>%
   #perfection!
   
   #save the updated cicada that now has county information added.
-  st_write(c, dsn = "copperheads/data/cicada/periodical_cicada_with_county.shp", layer = "Periodical_Cicada_Brood_W_County2", driver = "OpenFileGDB")
-
-#one-time process of adding county boundaries, no longer needed.
-      #load in the county boundaries for the whole US
-      #downloaded from: https://www.sciencebase.gov/catalog/item/5eaa545982cefae35a22231f
-      #path = "copperheads/data/GovernmentUnits_National_GDB.gdb"
-      #bigmap <- st_read(dsn = path, layer = "GU_CountyOrEquivalent")
-
-      #c <- left_join(cicada, bigmap, by = c("ST_CNTY_CODE" = "STCO_FIPSCODE"))
-
-      #save the updated cicada that now has county information added.
-      #st_write(c, dsn = "copperheads/data/cicada/periodical_cicada_with_county.gdb", layer = "Periodical_Cicada_Brood_W_County", driver = "OpenFileGDB")
-
-      #plot(c) #yep, works!
-#doesn't look quite the same as from the oak paper, but I think it's an artifact of the crs. If that turns out to be wrong, then we need another county boundaries to work from.
+  #if this doesn't work with your computer, try another one. This worked fine on one of mine and then on my lab computer it said it couldn't build a .gdb (even after updating sf)
+  st_write(c, dsn = "copperheads/data/cicada/periodical_cicada_with_county.gdb", layer = "Periodical_Cicada_Brood_W_County", driver = "OpenFileGDB")
 
 #to load in and plot the new .gdb this created
 cicada <- st_read(dsn = "copperheads/data/cicada/periodical_cicada_with_county.gdb")

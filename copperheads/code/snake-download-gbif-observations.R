994fabf50b0bdfa558434fede80c3a4d2037a04f
IJBG <ivaragoulden@gmail.com>
2024-10-09 15:02:47 -0400

    perc copperheads within brood area

--- copperheads/code/snake-download-gbif-observations.R
+++ copperheads/code/snake-download-gbif-observations.R
@@ -15,6 +15,8 @@ gbif_filepath = "copperheads/data/snakes/gbif-download-inat-squamata.csv"
 squamata <- readr::read_tsv(gbif_filepath,
                 quote = "")
 
+#hey, this is excluding all snake observations that haven't been identified to family. Think about if that's a good way to do this - I think we could assume that unidentified snakes are not copperheads, and boost the power of saying how many snakes were observed? But also, these are all the research grade observations, so they MUST have been identified further. Anyway, this why there's a difference between the number of records available on iNat (500k) vs the number downloaded from gbif (300 something k)
+
 #make list of all snake families found on the iNat 'serpentes' page
 snake_list <- c(
   "Acrochordidae",

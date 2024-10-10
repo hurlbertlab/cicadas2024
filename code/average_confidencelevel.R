4867395b0c669ded7715110cc76cbcccc2e12566
Grace Layman <egrac@ad.unc.edu>
2024-10-10 15:28:05 -0400

    finished creating final data set for bird net results

--- code/average_confidencelevel.R
+++ code/average_confidencelevel.R
@@ -27,3 +27,25 @@ confidence_levels <- confidence_levels %>%
   mutate(studysite = str_extract(resultsfile, pattern = "JMill|UNC|EnoRiver|NCBG|PRidge")) %>%
   mutate(distance = str_extract(resultsfile, pattern = "25m|50m")) %>%
   mutate(date = str_extract(resultsfile, pattern = "[0-9][0-9][0-9][0-9]"))
+
+confirmed_calls = read.csv("data/birdcalltimes/BirdCallsTimes.csv") %>%
+  mutate(Date = recode(Date,
+                      '516' = '0516',
+                      '521' = '0521',
+                      '618' = '0618',
+                      '520' = '0520',
+                      '617' = '0617',
+                      '517' = '0517',
+                      '524' = '0524',
+                      '614' = '0614',
+                      '522' = '0522',
+                      '619' = '0619',
+                      '523' = '0523',
+                      '620' = '0620',
+                      ),
+         Location = str_remove(Location, ' '),
+         Bird.Call = ifelse(Bird.Call == 'Eastern Wood Pewee', 'Eastern Wood-Pewee', Bird.Call))
+
+birdnetresults = left_join(confirmed_calls, confidence_levels, by = c('Start.Time' = 'Start..s.', 'End.Time' = 'End..s.', 'Bird.Call' = 'Common.name', 'Date' = 'date', 'Location' = 'site', 'Distance' = 'distance'))
+
+write.csv(birdnetresults,"data/birdnetresults.csv",row.names= FALSE)

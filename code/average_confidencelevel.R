8fd5ff4875e64e892fad79830a0e55a4a1038b3e
Grace Layman <egrac@ad.unc.edu>
2024-10-10 12:23:46 -0400

    finishing combining all of the results files into one file

--- code/average_confidencelevel.R
+++ code/average_confidencelevel.R
@@ -1,55 +1,29 @@
 #creating data frame for average confidence level of each bird species
 library(dplyr)
+library(stringr)
 
 
 sites = c("eno", "jmill", "ncbg", "pridge", "unc")
 
-confidence_levels = data.frame(site = NULL,
-                               file = NULL,
-                               start = NULL,
-                               end = NULL,
-                               scientific_name = NULL, 
-                               common_name = NULL,
-                               confidence = NULL)
+confidence_levels = read.csv(paste0("data/birdcallsdata/", "eno", "/", "EnoRiver_25m_0516.BirdNET.results.csv")) %>%
+  mutate(Common.name = "ex") %>%
+  mutate(site = "eno",
+         resultsfile = "anything")
 
 for(s in 1:length(sites)) {
   
  files <- list.files(paste0("data/birdcallsdata/", sites[s]))
   
   for(f in 1:length(files)) {
-    temp_file <- read.csv(files[f])
-    confidence_levels <- rbind(condfidence_levels, temp_file)
+    temp_file <- read.csv(paste0("data/birdcallsdata/", sites[s], "/", files[f])) %>%
+      mutate(site = sites[s],
+             resultsfile = files[f])
+    confidence_levels <- rbind(confidence_levels, temp_file)
   }
 }
 
-#confidence_levels = data.frame(site = "ex",
-                               file = "ex",
-                               start = 1,
-                               end = 1, 
-                               scientific_name = "ex", 
-                               common_name = "ex",
-                               confidence = 1)
-
-for (s in sites) {
-  
-  files = list.files(paste0("data/birdcallsdata", sites[s]))
-  
-  for (f in files)
-    file <- read.csv(f)
-  
-}
-#need to join sheet with BirdNET results to sheet with call times on it
-wd <- "Z:/Databases/CaterpillarsCount/AudioMoth/BirdNetResults"
-setwd("Z:/Databases/CaterpillarsCount/AudioMoth/BirdNetResults")
-confirmed_calls = read.csv(paste(wd,"/bird_calls_confirmed.csv", sep=""))
-#confirmed_calls = read.csv("bird_calls_confirmed")
-#need to create a read.csv line of code that will go through each excel sheet with results and rbinds them.
-
-
-# Joining by two columns 
-df = df1 %>%
-  left_join(df2, by = c('Start', 'SpeciesCall'))
-
-df = df1 %>%
-  left_join(df2, by = c('Start' = 'StartTime', 
-                        'SpeciesCall' = 'Species'))
+confidence_levels <- confidence_levels %>%
+  filter(Common.name != "ex") %>%
+  mutate(studysite = str_extract(resultsfile, pattern = "JMill|UNC|EnoRiver|NCBG|PRidge")) %>%
+  mutate(distance = str_extract(resultsfile, pattern = "25m|50m")) %>%
+  mutate(date = str_extract(resultsfile, pattern = "[0-9][0-9][0-9][0-9]"))

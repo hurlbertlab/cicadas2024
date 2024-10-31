09fb1e5d29ed92c962d078b0028952d4452727ae
IJBG <ivaragoulden@gmail.com>
2024-10-31 11:35:57 -0400

    linear model example

---
+++ code/linear_model_example.R
@@ -0,0 +1,28 @@
+#generate fake temperature data and run a linear model with it.
+
+library(dplyr)
+library(stringr)
+
+calls <- read.csv("data/cicada_output.csv") %>%
+  mutate(temp = case_when(
+    max_cicada_amp < .1 ~ 50,
+    max_cicada_amp < .3 ~ 60,
+    max_cicada_amp < .6 ~ 80,
+    TRUE ~ 90
+  ))
+
+#run a linear model, effect of temperature on amplitude
+m1 <- lm(max_cicada_amp ~ temp, data = calls)
+
+summary(m1)
+r2 <- .88
+
+plot(x = calls$temp, y = calls$max_cicada_amp)
+abline(m1)
+
+notes <- read.csv("data/CC!samplingnotes.csv") %>%
+  filter(str_detect(date, "2024")) %>%
+  mutate(temp = case_when(
+    str_detect(notes, "pattern you want" ~ 70)
+  ))
+

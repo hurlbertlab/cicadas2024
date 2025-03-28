e4963b7da06faf26c949ca951793400ceed1cf4f
InfringedBiologist <smith96@unc.edu>
2025-03-28 14:36:05 -0400

    created site list
    created seperate data for site colors

---
+++ site_names_colors.R
@@ -0,0 +1,40 @@
+site_names <- c(
+  "UNC Chapel Hill Campus",
+  "NC Botanical Garden",
+  "Eno River State Park",
+  "Prairie Ridge Ecostation",
+  "Triangle Land Conservancy - Johnston Mill Nature Preserve"
+)
+
+short_names <- c(
+  "UNC Chapel Hill Campus" = "UNC Campus",
+  "NC Botanical Garden" = "NCBG",
+  "Eno River State Park" = "Eno River",
+  "Prairie Ridge Ecostation" = "Prairie Ridge",
+  "Triangle Land Conservancy - Johnston Mill Nature Preserve" = "Johnston Mill"
+)
+
+site_colors <- c(
+  "UNC Chapel Hill Campus" = "#0072B2",
+  "NC Botanical Garden" = "#D55E00",
+  "Eno River State Park" = "black",
+  "Prairie Ridge Ecostation" = "#CC79A7",
+  "Triangle Land Conservancy - Johnston Mill Nature Preserve" = "yellow3"
+)
+
+site_shapes <- c(
+  "UNC Chapel Hill Campus" = 16,
+  "NC Botanical Garden" = 17,
+  "Eno River State Park" = 8,
+  "Prairie Ridge Ecostation" = 15,
+  "Triangle Land Conservancy - Johnston Mill Nature Preserve" = 18  
+)
+
+abbrev_names <- c(
+  "UNC Chapel Hill Campus" = "UNC",
+  "NC Botanical Garden" = "NCBG",
+  "Eno River State Park" = "ERSP",
+  "Prairie Ridge Ecostation" = "PRE",
+  "Triangle Land Conservancy - Johnston Mill Nature Preserve" = "JM"
+)
+

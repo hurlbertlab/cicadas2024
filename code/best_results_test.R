12f475a1f1eaf1affb915f9b612a2e94bdc72bd3
Grace Layman <egrac@ad.unc.edu>
2025-04-09 12:46:02 -0400

    plots for poster

--- code/best_results_test.R
+++ code/best_results_test.R
@@ -33,7 +33,7 @@ lm_cicada <- function(df = highestConf,
 
 summary(m1)
 m1$fitted.values
-m1 <- lm_cicada(birdsvcicada, "Yellow-billed Cuckoo")
+m1 <- lm_cicada(highestConf, "Yellow-billed Cuckoo")
 abline(m1, col = "blue")
 text(x = min(.10), y = max(.71), labels = paste("R² =", round(r_squared, 3)), pos = 4, col = 'blue')
 
@@ -43,7 +43,7 @@ tidied <- tidy(m1) %>%
   mutate(common_name = chosen_species) #add species column
 write.csv(tidied, "data/results_confvspecies/resultsBGGN.csv")
 m1 <- lm_cicada(highestConf, "Blue-Gray Gnatcatcher")
-
+summary(m1)
 #now I want to model the interaction plots again
 #modeling an interaction plot
 i_plot <- function(df= highestConf,
@@ -73,3 +73,21 @@ MODO
 YBCU <- i_plot(bird = "Yellow-billed Cuckoo")
 YBCU
 
+for (bird in unique(highestConf$Bird.Call)) {
+  lm_cicada <- function(df = highestConf,
+                        chosen_species = bird) {
+    #filter to just chosen species 
+    df <- df %>%
+      filter(Bird.Call == chosen_species)
+    #run linear model
+    m1 <- lm(Confidence ~ Distance + mean_noise + Distance*mean_noise , data = df)
+    plot(x = df$mean_noise, y = df$Confidence,
+         xlab = "Mean Cicada Amplitude", 
+         ylab = "Confidence Score",
+         main = paste("Mean Cicada Amplitude vs Confidence Score for", chosen_species))
+    r_squared <-summary(m1)$r.squared
+    
+    return(m1)
+    
+  }
+}

31a5b5cfbc055daaf639ad6b5c85bcd99cd98d04
InfringedBiologist <smith96@unc.edu>
2025-04-28 14:10:38 -0400

    Circle Color
    Corrected the circle color issue. red indicates circles where caterpillars were deployed.

--- code/CicadaMap.R
+++ code/CicadaMap.R
@@ -81,17 +81,20 @@ Circles <- data.frame(
   latitude = c(36.0740347603, 36.0744491933, 36.0751177629, 36.0761118226, 36.0768227486, 36.0773659175, 36.0786444282, 36.0790969042),
   longitude = c(-79.0076410258, -79.0070628588, -79.0073105049, -79.0081902695, -79.0085027469, -79.0089654279, -79.0083854581, -79.0073614669))
 
-#
 Eno.map <- get_stadiamap(ERSP.area, zoom = 16, maptype = "stamen_terrain")
 
-#Eno River map with circles
+# Adding a new column to for circle color
+Circles$color <- ifelse(Circles$Circle %in% c("Circle 1", "Circle 8"), "black", "red")
+
+# Eno River map 
 ggmap(Eno.map) +
-  geom_point(data = Circles, aes(x = longitude, y = latitude), color = "black", pch = 19, size = 4, face = "bold") +
+  geom_point(data = Circles, aes(x = longitude, y = latitude, color = color), 
+             pch = 19, size = 4, face = "bold", show.legend = FALSE) +
   geom_label_repel(data = Circles, aes(x = longitude, y = latitude, label = Circle), 
                    color = "black", size = 7, box.padding = 1.1) +
   labs(x = "Longitude", y = "Latitude") +
   theme_minimal() +
   theme(
-axis.title = element_text(size = 20, face = "bold"),
-axis.text = element_text(size = 15))
-
+    axis.title = element_text(size = 20, face = "bold"),
+    axis.text = element_text(size = 15)) +
+  scale_color_manual(values = c("black" = "black", "red" = "red"))
